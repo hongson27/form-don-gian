64c24c36c415221a1b26b3db50b128162a6aa142
Hong Son <dohongson27@gmail.com>
2024-04-15 21:21:33 +0700

    them file

---
+++ phep-cong.js
@@ -0,0 +1,6 @@
+function result() {
+    let a=+document.getElementById("aa").value;
+        let b =+document.getElementById("bb").value;
+    let sum= a+b;
+    document.getElementById("result").innerHTML(sum);
+}
